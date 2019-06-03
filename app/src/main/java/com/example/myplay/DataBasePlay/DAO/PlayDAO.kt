df3da648df64f2d286e18ea1637866a9c905ba8f
Carlos Ruiz <00095417@uca.edu.sv>
2019-06-02 22:41:37 -0600

    creando dao

---
+++ app/src/main/java/com/example/myplay/DataBasePlay/DAO/PlayDAO.kt
@@ -0,0 +1,20 @@
+package com.example.myplay.DataBasePlay.DAO
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.myplay.DataBasePlay.Entities.Play
+
+@Dao
+interface PlayDAO {
+
+    @Query("SELECT * FROM Play order by Id_Play ASC")
+    fun getAllPlay(): LiveData<List<Play>>
+
+    @Insert
+    suspend fun insertPartido(play: Play)
+
+    @Query("DELETE FROM Play")
+    fun deleteAllPlay()
+}
